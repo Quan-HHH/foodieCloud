c24d622d725b874457be80b8180c59614a7ce1c4
quanjiaxin <1411514716@qq.com>
2021-03-31 15:13:01 +0800

    feat: 首页详情

--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -1,18 +1,43 @@
-// miniprogram/pages/detail/detail.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    id: null,
+    itemDetail: {},
+    footer: [
+      {
+        icon: "icon-daka",
+        text: "去打卡"
+      },
+      {
+        icon: "icon-shipin",
+        text: "传视频"
+      },
+      {
+        icon: "icon-zhaopian",
+        text: "传照片"
+      },
+      {
+        icon: "icon-icon-test",
+        text: "写评价"
+      },
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const { id } = options
+    const itemDetail = app.globalData.foodList.find(({poiid}) => poiid === id )
+    console.log(itemDetail)
+    this.setData({
+      id,
+      itemDetail,
+    })
   },
 
   /**
