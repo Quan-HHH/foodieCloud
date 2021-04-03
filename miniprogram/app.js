7dfe41732c86621131f80314e3a759be59069e7f
zongwenn <365731679@qq.com>
2021-04-03 22:44:28 +0800

    feat: mine

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -9,7 +9,7 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        env: 'quanjiaxin1010-7gfvjtzicb90119c',
+        env: 'zongwen-3gvd3nim8be4790f',
         traceUser: true,
       })
     }
@@ -48,5 +48,23 @@ App({
     this.globalData = {
       userInfo: null
     }
+  },
+
+  //获取当前时间,返回时间格式：2021-04-13 21:43:36
+  getNowFormatDate: function () {
+    var date = new Date();
+    var seperator1 = "-";
+    var seperator2 = ":";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+      month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+     strDate = "0" + strDate;
+    }
+    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate +
+     " " + date.getHours() + seperator2 + date.getMinutes() +seperator2 + date.getSeconds();
+    return currentdate;
   }
 })
--- miniprogram/pages/community/community.js
+++ miniprogram/pages/community/community.js
@@ -1,66 +1,35 @@
-// miniprogram/pages/community/community.js
+let app = getApp();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    dataList: []
+  },
+  onLoad() {
+    let that = this;
+    wx.cloud.database().collection('timeline')
+      .orderBy('createTime', 'desc') //按发布视频排序
+      .get({
+        success(res) {
+          console.log("请求成功", res)
+          that.setData({
+            dataList: res.data
+          })
+        },
+        fail(res) {
+          console.log("请求失败", res)
+        }
+      })
+  },
+  // 预览图片
+  previewImg: function(e) {
+    let imgData = e.currentTarget.dataset.img;
+    console.log("eeee", imgData[0])
+    console.log("图片s", imgData[1])
+    wx.previewImage({
+      //当前显示图片
+      current: imgData[0],
+      //所有图片
+      urls: imgData[1]
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
